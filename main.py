8a4cd2a0185069d4db270e04da370fb21b16eddf
Atli-A <atli.nikulas@gmail.com>
2022-08-22 17:02:38 -0700

    Added initial code

---
+++ main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import sys
+import datetime
+import os
+import json
+
+path = sys.argv[1]
+
+subdirs = os.listdir(path)
+assert("messages" in subdirs)
+
+path += "/messages/"
+#print(os.listdir(path))
+
+try:
+    names = json.loads(open(path + "index.json").read())
+except Exception:
+    print("could not find %s" % path + "index.json")
+    sys.exit(1)
+
+# generate list of dms
+#     channel id : name
+dms = {}
+for i in os.listdir(path):
+    if os.path.isdir(path + i): 
+        if json.loads(open(path + i + "/channel.json").read())["type"] == 1:
+            dms[i] = names[i[1:]]
+
+
+# for each dm read and plot
+for k, v in dms.items():
+    msg_total = []
+    timestamp = []
+    msgs = open(path + k + "/messages.csv")
+    msgs = msgs.read().split(",\n")[:-1]
+    msgs.reverse()
+    for i in msgs: # skip first line
+        try:
+            date = i.split(",")[1]
+            timestamp.append(datetime.datetime.fromisoformat(date))
+        except Exception:
+            print("hi")
+            continue
+        if len(msg_total) == 0:
+            msg_total.append(1)
+        else:
+            msg_total.append(msg_total[-1] + 1)
+
+    plt.plot(timestamp, msg_total, "-")
+plt.show()
