b0ff91aa058c735b07a8d0664c1bad89bcfa1371
Atli-A <atli.nikulas@gmail.com>
2022-08-23 14:55:07 -0700

    Increased usability & added README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# Graphcord
+Graphcord allows you to take your discord data and graph your messages over time
+
+## Download
+```sh
+git clone https://github.com/Atli-A/Graphcord.git
+
+cd Graphcord
+```
+
+## Usage
+Make sure you have python3 installed as well as matplotlib
+
+To run graphcord download it and run `./graphcord.py path/to/your/data/package/zip`
--- graphcord.py
+++ graphcord.py
@@ -6,63 +6,80 @@ import sys
 import datetime
 import os
 import json
+import tempfile
+import zipfile
 
 def err(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
+def read(path):
+    if "messages" not in os.listdir(path):
+        err("No \"messages\" directory found in file")
+        sys.exit(1)
+
+    path = os.path.join(path, "messages/")
+
+    try:
+        names = json.loads(open(path + "index.json").read())
+    except Exception:
+        err("Could not find %s" % path + "index.json")
+        sys.exit(1)
+
+    # generate list of dms
+    #     channel id : name
+    print("Collecting Names")
+    dms = {}
+    for i in os.listdir(path):
+        if os.path.isdir(path + i): 
+            if json.loads(open(path + i + "/channel.json").read())["type"] == 1:
+                dms[i] = names[i[1:]]
+
+    print("Reading DMs")
+    # for each dm read and plot
+    for k, v in dms.items():
+        msg_total = []
+        timestamp = []
+        msgs = open(path + k + "/messages.csv")
+        msgs = msgs.read().split(",\n")[1:]
+        msgs.reverse()
+        for i in msgs: # skip first line
+            try:
+                date = i.split(",")[1]
+                timestamp.append(datetime.datetime.fromisoformat(date))
+            except Exception:
+                continue
+            if len(msg_total) == 0:
+                msg_total.append(1)
+            else:
+                msg_total.append(msg_total[-1] + 1)
+        if len(msgs) > 1000:
+            plt.plot(timestamp, msg_total, "-", label=v)
+
+    print("Plotting")
+    plt.legend( loc="upper left")
+
+    plt.ylabel("Messages")
+    plt.xlabel("Date")
+    plt.show()
+
+
+
+
 if len(sys.argv) != 2:
-    err("Expects one argument: the path to the discord data downloaded(unzipped)")
+    err("Expects one argument: the path to the discord data download")
     sys.exit(1)
 
 path = sys.argv[1]
 
-if not os.path.isdir(path):
-    err("No directory %s" % path)
+if not os.path.isfile(path):
+    err("No file %s" % path)
     sys.exit(1)
 
-if "messages" not in os.listdir(path):
-    err("No \"messages\" directory found in %s" % path)
-    sys.exit(1)
+zf = zipfile.ZipFile(path, "r")
 
-path = os.path.join(path, "messages/")
-
-try:
-    names = json.loads(open(path + "index.json").read())
-except Exception:
-    err("could not find %s" % path + "index.json")
-    sys.exit(1)
-
-# generate list of dms
-#     channel id : name
-dms = {}
-for i in os.listdir(path):
-    if os.path.isdir(path + i): 
-        if json.loads(open(path + i + "/channel.json").read())["type"] == 1:
-            dms[i] = names[i[1:]]
+with tempfile.TemporaryDirectory() as tf:
+    print("Extracting")
+    zf.extractall(tf)
+    read(tf)
 
 
-# for each dm read and plot
-for k, v in dms.items():
-    msg_total = []
-    timestamp = []
-    msgs = open(path + k + "/messages.csv")
-    msgs = msgs.read().split(",\n")[1:]
-    msgs.reverse()
-    for i in msgs: # skip first line
-        try:
-            date = i.split(",")[1]
-            timestamp.append(datetime.datetime.fromisoformat(date))
-        except Exception:
-            continue
-        if len(msg_total) == 0:
-            msg_total.append(1)
-        else:
-            msg_total.append(msg_total[-1] + 1)
-    if len(msgs) > 1000:
-        plt.plot(timestamp, msg_total, "-", label=v)
-
-plt.legend( loc="upper left")
-
-plt.ylabel("Messages")
-plt.xlabel("Date")
-plt.show()
